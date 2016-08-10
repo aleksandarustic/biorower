c4befcc3860f6bc858104b0099e93ea49705fe46
tomke94 <gglavaski@yahoo.com>
2016-08-10 11:09:50 +0200

    Prikaz avatara u header-u i sidebar-u za settings stranu, edit navbar-a(fajl myframe.blade.php)

--- backend/resources/views/layouts/myframe.blade.php
+++ backend/resources/views/layouts/myframe.blade.php
@@ -127,52 +127,15 @@
 		<!-- Navbar Right Menu -->
 		<div class="navbar-custom-menu">
 			<ul class="nav navbar-nav">
+
 				<!-- Messages: style can be found in dropdown.less-->
-				<li class="dropdown messages-menu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-envelope-o"></i> <span class="label label-success">4</span> </a>
-					<ul class="dropdown-menu">
-						<li class="header">You have 4 messages</li>
-						<li>
-							<!-- inner menu: contains the actual data -->
-							<ul class="menu">
-								<li><!-- start message -->
-									<a href="#">
-										<div class="pull-left"> <img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image"> </div>
-										<h4> Support Team <small><i class="fa fa-clock-o"></i> 5 mins</small> </h4>
-										<p class="h2-subhead">Why not buy a new awesome theme?</p>
-									</a> </li>
-								<!-- end message -->
-								<li> <a href="#">
-										<div class="pull-left"> <img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image"> </div>
-										<h4> AdminLTE Design Team <small><i class="fa fa-clock-o"></i> 2 hours</small> </h4>
-										<p class="h2-subhead">Why not buy a new awesome theme?</p>
-									</a> </li>
-								<li> <a href="#">
-										<div class="pull-left"> <img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image"> </div>
-										<h4> Developers <small><i class="fa fa-clock-o"></i> Today</small> </h4>
-										<p class="h2-subhead">Why not buy a new awesome theme?</p>
-									</a> </li>
-								<li> <a href="#">
-										<div class="pull-left"> <img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image"> </div>
-										<h4> Sales Department <small><i class="fa fa-clock-o"></i> Yesterday</small> </h4>
-										<p class="h2-subhead">Why not buy a new awesome theme?</p>
-									</a> </li>
-								<li> <a href="#">
-										<div class="pull-left"> <img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image"> </div>
-										<h4> Reviewers <small><i class="fa fa-clock-o"></i> 2 days</small> </h4>
-										<p class="h2-subhead">Why not buy a new awesome theme?</p>
-									</a> </li>
-							</ul>
-						</li>
-						<li class="footer"><a href="messages.html">See All Messages</a></li>
-					</ul>
-				</li>
+			
 				<!-- Notifications: style can be found in dropdown.less -->
-				<li class="dropdown notifications-menu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-bell-o"></i> <span class="label label-warning">10</span> </a>
+			<!--	<li class="dropdown notifications-menu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-bell-o"></i> <span class="label label-warning">10</span> </a>
 					<ul class="dropdown-menu">
 						<li class="header">You have 10 notifications</li>
 						<li>
-							<!-- inner menu: contains the actual data -->
-							<ul class="menu">
+											<ul class="menu">
 								<li> <a href="#"> <i class="fa fa-users text-aqua"></i> 5 new members joined today </a> </li>
 								<li> <a href="#"> <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the page and may cause design problems </a> </li>
 								<li> <a href="#"> <i class="fa fa-users text-red"></i> 5 new members joined </a> </li>
@@ -182,39 +145,38 @@
 						</li>
 						<li class="footer"><a href="#">View all</a></li>
 					</ul>
-				</li>
+				</li> 
+			-->
 				<!-- User Account Menu -->
-				<li class="dropdown user user-menu">
-					<!-- Menu Toggle Button -->
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<!-- The user image in the navbar-->
-						<img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
-						<!-- hidden-xs hides the username on small devices so only the image appears. -->
-						<span class="hidden-xs">{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</span> </a>
-					<ul class="dropdown-menu">
-						<!-- The user image in the menu -->
-						<li class="user-header"> <img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
-							<p class="h2-subhead"> {{ Auth::user()->first_name.' '.Auth::user()->last_name }} <small>Member since {{ date('M Y', strtotime(Auth::user()->created_at)) }}</small> </p>
-						</li>
-						<!-- Menu Body -->
-						<li class="user-body">
-							<div class="act-block text-left">
-								<a href="{{ url('/profile/edit') }}" class="act-list"><i class="fa fa-user"></i> Username</a>
-							</div>
-							<div class="act-block text-left">
-								<a href="{{ url('/friends') }}" class="act-list"><i class="fa fa-users"></i> Friends</a>
-							</div>
-							<div class="act-block text-left">
-								<a href="{{ url('/messages') }}" class="act-list"> <i class="fa fa-envelope"></i> Messages</a>
-							</div>
-						</li>
-						<!-- Menu Footer-->
-						<li class="user-footer">
-							<div class="pull-left"> <a href="{{ url('/profile/edit') }}" class="btn btn-default btn-flat">Profile</a> </div>
-							<div class="pull-right"> <a href="{{ url('/profile/logout') }}" class="btn btn-default btn-flat">Sign out</a> </div>
-						</li>
-					</ul>
-				</li>
+                <li class="dropdown user user-menu">
+                    <!-- Menu Toggle Button -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <!-- The user image in the navbar-->
+                        <img src="{{ URL::asset(Auth::user()->profile->image->name) }}" class="user-image" alt="User Image">
+
+                        <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                        <span class="hidden-xs">{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</span> 
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <!-- The user image in the menu -->
+                        <li class="user-header">
+                        <br>
+                            <a href="{{ url('/profile/edit') }}">
+                                    <img src="{{ URL::asset(Auth::user()->profile->image->name) }}" class="user-image" alt="User Image">                
+                            </a>
+
+                            <p class="h2-subhead"> {{ Auth::user()->first_name.' '.Auth::user()->last_name }} <small>Member since {{ date('M Y', strtotime(Auth::user()->created_at)) }}</small> </p>
+
+
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left"> <a href="{{ url('/profile/edit') }}" class="btn btn-default btn-flat">Profile</a> </div>
+                            <div class="pull-right"> <a href="{{ url('/profile/logout') }}" class="btn btn-default btn-flat">Sign out</a> </div>
+                        </li>
+                    </ul>
+                </li>
 			</ul>
 		</div>
 
@@ -243,12 +205,10 @@
 		<!-- Sidebar user panel (optional) -->
 		<div class="user-panel">
 			<div class="pull-left image">
-				<img src="{{ URL::asset('images/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+				<img src="{{ URL::asset(Auth::user()->profile->image->name) }}" class="user-image" alt="User Image">
 			</div>
 			<div class="pull-left info">
 				<p>{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</p>
-				<p>Memeber:
-					{{ date('Y-m-d', strtotime(Auth::user()->created_at)) }}</p>
 				<!-- Status -->
 			</div>
 		</div>
